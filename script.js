28ebe33b958b39b9c58861c5a24c1deb44b68a24
Truong Vinh Kien <kientruong8543@gmail.com>
2025-11-27 01:18:37 +0100

    show all ep and layout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +7,32 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    const episodeElem = document.createElement("div");
+    episodeElem.classList.add("episode");
+
+    const titleElem = document.createElement("h2");
+
+    const seasonNum = episode.season.toString().padStart(2, "0");
+    const episodeNum = episode.number.toString().padStart(2, "0");
+    titleElem.textContent = `${episode.name} - S${seasonNum}E${episodeNum}`;
+
+    episodeElem.appendChild(titleElem);
+
+    const imgElem = document.createElement("img");
+    imgElem.src = episode.image.medium;
+    episodeElem.appendChild(imgElem);
+
+    const summaryElem = document.createElement("div");
+    summaryElem.innerHTML = episode.summary;
+    episodeElem.appendChild(summaryElem);
+
+    rootElem.appendChild(episodeElem);
+  });
+
+  const creditElem = document.createElement("footer");
+  creditElem.innerHTML = `Data sourced from <a href="https://tvmaze.com/">TVMaze.com</a>`;
+  rootElem.appendChild(creditElem);
 }
 
 window.onload = setup;
